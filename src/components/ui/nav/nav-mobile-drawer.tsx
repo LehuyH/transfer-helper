7545ba7a666999cc8311a8eb568c91c161be693b
LehuyH <44150458+LehuyH@users.noreply.github.com>
2024-08-30 22:56:39 -0700

    feat: mockup CTA to select majors

--- src/components/ui/nav/nav-mobile-drawer.tsx
+++ src/components/ui/nav/nav-mobile-drawer.tsx
@@ -4,6 +4,8 @@ import { ChevronRight, MenuIcon } from "lucide-react";
 import { Button } from "../button";
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion"
+import { links } from "./nav-desktop";
+import Link from "next/link";
 
 export default function NavMobileDrawer() {
     const [showMenu, setShowMenu] = useState(false)
@@ -15,12 +17,12 @@ export default function NavMobileDrawer() {
                 {
                     showMenu &&
                     <motion.div
-                        className="fixed z-10 min-h-screen p-4 left-0 top-0 w-full bg-slate-500"
+                        className="fixed z-10 min-h-screen p-4 left-0 top-0 w-full bg-black/50 backdrop-blur-sm"
                         initial={{ x: '100%' }}
                         animate={{ x: 0 }}
                         exit={{ x: '100%' }}
                         transition={{
-                            ease:'easeInOut'
+                            ease: 'easeInOut'
                         }}
                     >
                         <aside className="w-full flex justify-end">
@@ -28,6 +30,19 @@ export default function NavMobileDrawer() {
                                 onClick={() => setShowMenu(!showMenu)}
                                 Icon={ChevronRight} />
                         </aside>
+
+                        <ul className="space-y-4 py-4">
+                            {
+                                links.map((link, index) => (
+                                    <Link key={index} href={link.href} className="block w-full">
+                                        <Button Icon={link.Icon} className="w-full">
+                                            {link.label}
+                                        </Button>
+                                    </Link>
+                                ))
+                            }
+                        </ul>
+
                     </motion.div>
                 }
             </AnimatePresence>
