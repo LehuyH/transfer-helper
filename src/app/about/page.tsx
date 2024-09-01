22aaa1007a92ea5d6f40eadec2f0f8d009cf2644
Albert <ayang04@zohomail.com>
2024-08-31 20:09:49 -0700

    chore: fix the about page because idk whats happening lolol

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,13 @@
-export default function About(){
-    return (
-        <p>HI</p>
-    )
+"use client"
+
+import React from 'react';
+import { Button } from '@mui/material';
+
+
+export default function About() {
+  const handleBackClick = () => {
+    window.location.href = 'https://transferhelper.pages.dev/';
+  };
+
+  
 }
