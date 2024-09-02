27f57965ec0547879c86bde252cebc6ebe3673a3
Albert <ayang04@zohomail.com>
2024-09-02 01:34:34 -0700

    +updated discrete advertising

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,28 +14,28 @@ export default async function HomePage() {
   }))
 
   return (
-    <main className="h-full flex flex-col gap-12 justify-center items-center px-4 s">
-      <div className="text-center space-y-2">
-        <h1 className="text-4xl tracking-wider md:text-6xl font-bold">
-          Transfer Helper
-        </h1>
-        <h2 className="text-lg md:text-xl">
-          Get your California Community College transfer plan in <b>minutes</b>
-        </h2>
-      </div>
-      <aside className="max-w-4xl w-full mx-auto px-6 space-y-4">
-        <p className="space-y-1">
-          <Label htmlFor="homeCC">
-            Your Primary Community College
-          </Label>
-          <CollegePicker options={options}/>
-        </p>
-        <aside className="text-center">
-          <Link href="https://studykit.app" className="hover:underline text-sm opacity-75" target="_blank">
-          🦊 Need Free Study Tools for College? Check out StudyKit! (also made by me)
-          </Link>
+    <div className="h-full flex flex-col justify-between items-center px-4">
+      <main className="flex-grow flex flex-col gap-12 justify-center items-center w-full">
+        <div className="text-center space-y-2">
+          <h1 className="text-4xl tracking-wider md:text-6xl font-bold">
+            Transfer Helper
+          </h1>
+          <h2 className="text-lg md:text-xl">
+            Get your California Community College transfer plan in <b>minutes</b>
+          </h2>
+        </div>
+        <aside className="max-w-4xl w-full mx-auto px-6 space-y-4">
+          <p className="space-y-1">
+            <Label htmlFor="homeCC">Your Primary Community College</Label>
+            <CollegePicker options={options} />
+          </p>
         </aside>
-      </aside>
-    </main>
+      </main>
+      <footer className="w-full text-center mt-8">
+        <Link href="https://studykit.app" className="hover:underline text-sm opacity-75" target="_blank">
+          🦊 Want to see more educational tools from us? Check out StudyKit!
+        </Link>
+      </footer>
+    </div>
   );
 }
